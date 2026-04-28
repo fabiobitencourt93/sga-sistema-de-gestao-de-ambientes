cf317c04602a23a11ea3eb315014735ffede4d07
fabiobitencourt93 <fabiobitencourt.93@gmail.com>
2026-04-28 19:21:20 -0300

    28-04-2026

---
+++ index.js
@@ -0,0 +1,22 @@
+ const botao = document.getElementById("entrar")
+    const form = document.getElementById("loginForm")
+   
+
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault()
+
+        const email = document.getElementById("email").value
+        const senha = document.getElementById("senha").value
+
+        if(email === "" || senha === ""){
+            alert("Erro na validação")
+        }
+        else{
+            alert("Formulário Enviado!")
+
+            console.log({
+                email: email,
+                senha: senha
+            })
+        }
+    })
